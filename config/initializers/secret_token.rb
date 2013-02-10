ede6d9aaa85b4ae3b7fbd876b947a90440ec4363
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2013-02-11 02:05:37 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weirdtest::Application.config.secret_token = '7d877697d3e70c833684ee053a545f30306c245d61620434bf85aefb2f5cdf2afeb85d88923fd87f762dfe694d0a4d5dde4ffe21e27984778f4f1f711afbaf07'
